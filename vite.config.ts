1a591b04f226f732365d0e7a6a37ebc96cf7fae8
loick <loick.59252@gmail.Com>
2026-05-27 20:04:15 +0200

    Commit '2026-05-27' - '20:04:11' - cest le bord

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCase'
-    }
+  server: {
+    port: 5173, // Port par défaut
+    host: true // Permet l'accès depuis d'autres appareils du réseau local
   }
 })
